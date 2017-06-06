095e44c27fc0b0cfdd61eadce6f15ed406b9b09e
Betsi Loayza <betsi18_12@hotmail.com>
2017-06-06 16:13:23 -0500

    input

--- js/components/header.js
+++ js/components/header.js
@@ -4,8 +4,14 @@ const Header = (update) => {
   const header = $("<header></header>");
   const title = $("<span>Gas Finder</span>");
   const icon = $('<i class="fa fa-chevron-left" aria-hidden="true"></i>');
+  const input = $('<input placeholder="Ingresa tu distrito a buscar" class="input-busqueda"></input>')
+  const cuadroicon = $('<div class="iconos"></div>');
+  const iconlupa = $('<i class="fa fa-search" aria-hidden="true"></i>');
 
   header.append(title);
+  cuadroicon.append(iconlupa);
+  header.append(cuadroicon)
+  header.append(input);
 
   return header;
 }
