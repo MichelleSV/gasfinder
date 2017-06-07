4ca5a00f1b2cc6441114c83ba19ee8a1b4d5f9ed
Betsi Loayza <betsi18_12@hotmail.com>
2017-06-07 16:52:04 -0500

    mostrar filtro

--- js/utils/filters.js
+++ js/utils/filters.js
@@ -1,20 +1,22 @@
 'use strict';
 
 const filterByDistrict = (stations,query) => { /*objeto donde esta la informacion, input */
-
+let nombre;
   for (var i = 0; i < stations.length; i++) {
     const distrito = stations[i].district;
     if( distrito == query){
-    const nombre = stations[i].name;
+     nombre = stations[i].name;
 
-      console.log(query);
+      console.log(distrito);
       console.log(nombre);
     }
   }
   const busqueda = $('<div class="seleccion"></div>');
-  const lista = $('<p></p>');
+  const lista = $('<p>'+nombre+'</p>');
   busqueda.append(lista);
 
-  return busqueda;
-
+  const root1 = $('.root');
+  root1.append(busqueda);
+  console.log(root1);
+  return root1;
 }
